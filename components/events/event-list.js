829b1e299d938fe63fe39c5ef99936328eebc620
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-03 07:09:38 +1000

    Adding Dummy Data & Static Files, Adding Regular React Components, Adding More React Components & Connecting Components

---
+++ components/events/event-list.js
@@ -0,0 +1,20 @@
+import EventItem from './event-item'
+
+const EventList = ({ items }) => {
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          date={event.date}
+          location={event.location}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default EventList
