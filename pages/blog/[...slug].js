72a935433dc177948da9311400ebadccc5f1347c
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-02 18:18:42 +1000

    Pages & File-based Routing

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,15 @@
+import { Router, useRouter } from "next/router"
+
+const BlogPostsPage = () => {
+  const router = useRouter()
+  
+  console.log(router.query)
+  
+  return (
+    <div>
+      <h1>The Blog Posts</h1>
+    </div>
+  )
+}
+
+export default BlogPostsPage
