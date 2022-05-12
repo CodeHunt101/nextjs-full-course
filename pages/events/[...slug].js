613e5ca5e021484f332a6f9e6c0320ac13f377e8
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-12 20:30:05 +1000

    using SSR

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,17 +2,64 @@ import { useRouter } from 'next/router'
 import EventList from '../../components/events/event-list'
 import ResultsTitle from '../../components/events/results-title'
 import Button from '../../components/ui/button'
-import { getFilteredEvents } from '../../dummy-data'
+import { getFilteredEvents } from '../../helpers/api-util'
 import ErrorAlert from '../../components/ui/error-alert'
 
-const FilteredEventsPage = () => {
-  const router = useRouter()
+const FilteredEventsPage = ({ events, hasError, datee }) => {
+  // const router = useRouter()
 
-  const filterData = router.query.slug
+  // const filterData = router.query.slug
 
-  if (!filterData) {
-    return <p className="center">Loading...</p>
+  // if (!filterData) {
+  //   return <p className="center">Loading...</p>
+  // }
+
+  // const filteredYear = filterData[0]
+  // const filteredMonth = filterData[1]
+
+  // const numYear = +filteredYear
+  // const numMonth = +filteredMonth
+
+  if (hasError) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link={'/events'}>Show All Events</Button>
+        </div>
+      </>
+    )
   }
+
+  const filteredEvents = events
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link={'/events'}>Show All Events</Button>
+        </div>
+      </>
+    )
+  }
+
+  const date = new Date(datee.year, datee.month - 1)
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
+  )
+}
+
+export const getServerSideProps = async ({ params }) => {
+  const filterData = params.slug
 
   const filteredYear = filterData[0]
   const filteredMonth = filterData[1]
@@ -28,44 +75,29 @@ const FilteredEventsPage = () => {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>Invalid filter. Please adjust your values!</p>
-        </ErrorAlert>
-        <div className="center">
-          <Button link={'/events'}>Show All Events</Button>
-        </div>
-      </>
-    )
+    return {
+      props: { hasError: true },
+      // notFound: true,
+      // redirect: {
+      //   destination: '/error'
+      // }
+    }
   }
 
-  const filteredEvents = getFilteredEvents({
+  const filteredEvents = await getFilteredEvents({
     year: numYear,
     month: numMonth,
   })
 
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>No events found for the chosen filter!</p>
-        </ErrorAlert>
-        <div className="center">
-          <Button link={'/events'}>Show All Events</Button>
-        </div>
-      </>
-    )
+  return {
+    props: {
+      events: filteredEvents,
+      datee: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
   }
-
-  const date = new Date(numYear, numMonth - 1)
-
-  return (
-    <>
-      <ResultsTitle date={date} />
-      <EventList items={filteredEvents} />
-    </>
-  )
 }
 
 export default FilteredEventsPage
