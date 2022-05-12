943d2bd1f01cc961b9cf9ffab6fc1ca85ebe2dd1
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-13 07:21:58 +1000

    Working with the _app.js file and reusing logic inside components

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -34,9 +34,19 @@ const FilteredEventsPage = () => {
 
     setLoadedEvents(events)
   }, [data])
+  
+  let pageHeadData = (
+    <Head>
+      <title>Filtered Events</title>
+      <meta name={`A list of filtered events`} />
+    </Head>
+  )
 
   if (!loadedEvents) {
-    return <p className="center">Loading...</p>
+    return <>
+    {pageHeadData}
+    <p className="center">Loading...</p>
+    </>
   }
 
   const filteredYear = filterData[0]
@@ -44,6 +54,13 @@ const FilteredEventsPage = () => {
 
   const numYear = +filteredYear
   const numMonth = +filteredMonth
+
+  pageHeadData = (
+    <Head>
+      <title>Filtered Events</title>
+      <meta name={`All events for ${numMonth}/${numYear}`} />
+    </Head>
+  )
 
   if (
     isNaN(numYear) ||
@@ -56,6 +73,7 @@ const FilteredEventsPage = () => {
   ) {
     return (
       <>
+      {pageHeadData}
         <ErrorAlert>
           <p>Invalid filter. Please adjust your values!</p>
         </ErrorAlert>
@@ -77,6 +95,7 @@ const FilteredEventsPage = () => {
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <>
+      {pageHeadData}
         <ErrorAlert>
           <p>No events found for the chosen filter!</p>
         </ErrorAlert>
@@ -91,10 +110,7 @@ const FilteredEventsPage = () => {
 
   return (
     <>
-      <Head>
-        <title>Filtered Events</title>
-        <meta name={`All events for ${numMonth}/${numYear}`} />
-      </Head>
+    {pageHeadData}
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </>
