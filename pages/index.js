0408de4535478840e8cad87ecbc5817d8aabe6f4
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-14 14:58:13 +1000

    Repo preparation

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
-import Head from 'next/head'
-
-import EventList from '../components/events/event-list'
-import { getFeaturedEvents } from '../helpers/api-util'
-
-const HomePage = ({ featuredEvents }) => {
+function HomePage() {
   return (
-    <>
-      <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="Find a lot of great events" />
-      </Head>
-      <EventList items={featuredEvents} />
-    </>
-  )
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
 }
 
-export const getStaticProps = async () => {
-  const featuredEvents = await getFeaturedEvents()
-  return {
-    props: {
-      featuredEvents,
-    },
-    revalidate: 1800,
-  }
-}
-
-export default HomePage
+export default HomePage;
