b394f8069e74e55d0f6f1df59cfcb58bb54e1af3
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-14 17:45:38 +1000

    Sending requests and using API routes to get data

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,20 @@
 import fs from 'fs'
 import path from 'path'
 
+const buildFeedbackPath = () => {
+  return path.join(process.cwd(), 'data', 'feedback.json')
+}
+
+const extractFeedback = (filePath) => {
+  const fileData = fs.readFileSync(filePath)
+  const data = JSON.parse(fileData)
+  return data
+}
+
 const handler = (req, res) => {
+  const filePath = buildFeedbackPath()
+  const data = extractFeedback(filePath)
+
   if (req.method === 'POST') {
     console.log(req)
     const email = req.body.email
@@ -12,16 +25,12 @@ const handler = (req, res) => {
       email,
       text: feedbackText,
     }
-
     // store that in a database or in a file
-    const filePath = path.join(process.cwd(), 'data', 'feedbackText.json')
-    const fileData = fs.readFileSync(filePath)
-    const data = JSON.parse(fileData)
     data.push(newFeedback)
     fs.writeFileSync(filePath, JSON.stringify(data))
     res.status(201).json({ message: 'Sucess!!', feedback: newFeedback })
   } else {
-    res.status(200).json({ message: 'This works!' })
+    res.status(200).json({ feedback: data })
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([])
+
   const emailInputRef = useRef()
   const feedbackInputRef = useRef()
 
-  const submitFormHanlder = (e) => {
+  const submitFormHanlder = async (e) => {
     e.preventDefault()
     const enteredEmail = emailInputRef.current.value
     const enteredFeedback = feedbackInputRef.current.value
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    }
+
+    const response = await fetch('/api/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reqBody),
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+
+  const loadFeedbackHandler = async () => {
+    const response = await fetch('/api/feedback')
+    const data = await response.json()
+    setFeedbackItems(data.feedback)
   }
 
   return (
@@ -24,6 +47,15 @@ function HomePage() {
         </div>
         <button>Send Feedback</button>
       </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load feedbacks</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.email} - {item.text}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
