435b4320c04b4d4884ef04972fc04b81def92eeb
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-16 06:43:39 +1000

    Creating a new React Context, adding Context State and using Contaxt Data in Components

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,25 @@
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 
 import MainHeader from './main-header'
+import Notification from '../ui/notification'
+import NotificationContext from '../../store/notification-context'
 
-function Layout(props) {
+function Layout({ children }) {
+  const notificationCtx = useContext(NotificationContext)
+
+  const activeNotification = notificationCtx.notification
+
   return (
     <Fragment>
       <MainHeader />
-      <main>{props.children}</main>
+      <main>{children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   )
 }
