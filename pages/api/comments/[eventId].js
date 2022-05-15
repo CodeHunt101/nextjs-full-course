c4591fb55b22b35390d95cdd3259b96f358267f2
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-15 10:18:12 +1000

    Set up mongodb and using it to insert emails and insert and retrieve comments

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,11 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb'
+
+const handler = async (req, res) => {
   const eventId = req.query.eventId
+
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a28iu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
 
   if (req.method === 'POST') {
     const { email, name, text } = req.body
@@ -16,23 +22,34 @@ const handler = (req, res) => {
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     }
-    console.log(newComment)
+
+    const db = client.db()
+    const result = await db.collection('comments').insertOne(newComment)
+    console.log(result)
+
+    newComment.id = result.insertedId
+
     res.status(201).json({ message: 'Added comment', comment: newComment })
   }
 
   if (req.method === 'GET') {
-    const dummyList = [
-      { id: 'c1', name: 'Max', text: 'A first comment' },
-      { id: 'c2', name: 'Manuel', text: 'A second comment' },
-    ]
+    const db = client.db()
 
-    res.status(200).json({ comments: dummyList })
+    const documents = await db
+      .collection('comments')
+      .find()
+      .sort({ _id: -1 }) //-1 desc +1 asc
+      .toArray()
+
+    res.status(200).json({ comments: documents })
   }
+
+  client.close()
 }
 
 export default handler
--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,14 +1,23 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb'
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const userEmail = req.body.email
-    // res.status(200).json({ message: 'works' })
 
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email address' })
       return
     }
 
-    console.log(userEmail)
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a28iu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    )
+    const db = client.db()
+
+    await db.collection('newsletter').insertOne({ email: userEmail })
+
+    client.close()
+
     res.status(201).json({ message: 'Signed up!' })
   }
 }
