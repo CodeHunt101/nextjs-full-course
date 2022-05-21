434950d98a84194b9a259e5741c24e48435ad0b4
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-21 18:01:08 +1000

    Adding 'head' data, adding a _document.js file Using React Portals

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Hero from '../components/home-page/hero'
+import Head from 'next/head'
 import FeaturedPosts from '../components/home-page/featured-posts'
 import { getFeaturedPosts } from '../lib/posts-util'
 
 const HomePage = ({ posts }) => {
   return (
     <>
+      <Head>
+        <title>Max' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </>
