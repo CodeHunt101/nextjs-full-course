72477ab7d5e39b05ae3ac7d6e2e1b42a1e7bc202
Harold Torres Marino <haroldtm55@gmail.com>
2022-05-22 13:12:48 +1000

    Preparing files

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,7 @@
-import Hero from '../components/home-page/hero'
-import Head from 'next/head'
-import FeaturedPosts from '../components/home-page/featured-posts'
-import { getFeaturedPosts } from '../lib/posts-util'
+import StartingPageContent from '../components/starting-page/starting-page';
 
-const HomePage = ({ posts }) => {
-  return (
-    <>
-      <Head>
-        <title>Max' Blog</title>
-        <meta
-          name="description"
-          content="I post about programming and web development."
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={posts} />
-    </>
-  )
+function HomePage() {
+  return <StartingPageContent />;
 }
 
-export const getStaticProps = () => {
-  const featuredPosts = getFeaturedPosts()
-
-  return {
-    props: {
-      posts: featuredPosts,
-    },
-  }
-}
-
-export default HomePage
+export default HomePage;
